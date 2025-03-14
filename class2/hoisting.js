0b9e827d0f18dce85445b4eb85219278e75b3d60
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-03-14 22:13:08 +0530

    Enhance hoisting.js with additional examples and explanations of variable scope and scope chain

--- class2/hoisting.js
+++ class2/hoisting.js
@@ -11,3 +11,22 @@ console.log(fun); //prints undefined because fun is not hoisted
 var fun = () => {
   console.log("i am fun");
 };
+
+console.log(window); //prints window object because window is hoisted
+
+// js is a loosely typed language, so we can assign any type of value to a variable
+
+// scope = where the variable is accessible;
+function a() {
+  // var x = 11;
+  console.log(x);
+  function b() {
+    // var x = 12;
+    console.log(x);
+  }
+  b();
+}
+
+a();
+ 
+// scope chain  = when a variable is not found in the current scope, it goes to the parent scope to find the variable
