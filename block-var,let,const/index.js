a9ed3bd5ad8f3b3ab45e5fbecb47f3265bde02ad
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-03-15 18:58:02 +0530

    block scope with var, let, and const

---
+++ block-var,let,const/index.js
@@ -0,0 +1,8 @@
+{
+  var a = 10;
+  let aa = 10;
+  const aaa = 10;
+}
+console.log(a); // 10
+console.log(aa); // ReferenceError: aa is not defined
+console.log(aaa); // ReferenceError: aaa is not defined
