1420e5c259576356038288936a89232673fa5e87
shivam digiprod <lootforecast@gmail.com>
2025-03-21 19:09:23 +0530

    shadoing in js

--- class2/hoisting.js
+++ class2/hoisting.js
@@ -1,32 +1,42 @@
-console.log(f); //prints actual function because f is hoisted
-console.log(x); //prints undefined because x is not hoisted
-var x = 10;
+// console.log(f); //prints actual function because f is hoisted
+// console.log(x); //prints undefined because x is not hoisted
+// var x = 10;
 
-function f() {
-  console.log("i am f");
+// function f() {
+//   console.log("i am f");
+// }
+
+// console.log(fun); //prints undefined because fun is not hoisted
+
+// var fun = () => {
+//   console.log("i am fun");
+// };
+
+// console.log(window); //prints window object because window is hoisted
+
+// // js is a loosely typed language, so we can assign any type of value to a variable
+
+// // scope = where the variable is accessible;
+// function a() {
+//   // var x = 11;
+//   console.log(x);
+//   function b() {
+//     // var x = 12;
+//     console.log(x);
+//   }
+//   b();
+// }
+
+// a();
+
+// // scope chain  = when a variable is not found in the current scope, it goes to the parent scope to find the variable
+var a = 10;
+let b = 20;
+{
+  var a = 30;
+  let b = 40;
+  console.log(a);
+  console.log(b);
 }
-
-console.log(fun); //prints undefined because fun is not hoisted
-
-var fun = () => {
-  console.log("i am fun");
-};
-
-console.log(window); //prints window object because window is hoisted
-
-// js is a loosely typed language, so we can assign any type of value to a variable
-
-// scope = where the variable is accessible;
-function a() {
-  // var x = 11;
-  console.log(x);
-  function b() {
-    // var x = 12;
-    console.log(x);
-  }
-  b();
-}
-
-a();
- 
-// scope chain  = when a variable is not found in the current scope, it goes to the parent scope to find the variable
+console.log(a);
+console.log(b);
