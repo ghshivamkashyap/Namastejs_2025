88f9d22f35946ec786a395ef3ca38ca7dfad9481
shivam digiprod <lootforecast@gmail.com>
2025-03-30 12:26:29 +0530

    map, filter, and reduce

---
+++ map_filter_reduce/index.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 3, 4, 5];
+
+// map  - returns a new array with the results of calling a provided function on every element in the calling array.
+console.log(
+  arr.map((item) => {
+    return item * 2;
+  })
+);
+
+// filter  - creates a new array with all elements that pass the test implemented by the provided function.
+
+console.log(
+  arr.filter((item) => {
+    return item % 2 != 0;
+  })
+);
+
+// reduce - executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
+console.log(
+  arr.reduce((acc, curr) => {
+    console.log(`acc: ${acc}, curr: ${curr}`);
+    return acc * curr;
+  })
+);
---
+++ map_filter_reduce/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log(
+  arr.map((item) => {
+    return item * 2;
+  })
+);
