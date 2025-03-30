8b79ab23d0e63ac96fe16afa967bfb7ed05f931f
shivam digiprod <lootforecast@gmail.com>
2025-03-30 15:34:31 +0530

    promise final

--- callback_promise/index.js
+++ callback_promise/index.js
@@ -28,3 +28,18 @@ const pay = createorder()
   .catch((err) => {
     console.log("err: ", err);
   });
+
+  
+const userdata = await fetch("https://api.github.com/users/ghshivamkashyap")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return res.json();
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+    return null;
+  });
+
+console.log("userdata: ", userdata);
