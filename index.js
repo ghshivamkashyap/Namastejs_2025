1e010dd5f84f3a9a2ee58e0a26f947d2ea031266
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-05-24 15:03:36 +0530

    map, filter, reduce

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log("first");
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-setTimeout(() => {
-  console.log("second");
-}, 3000);
-while (true) true;
+console.log(arr.filter((x) => x >= 6));
+
+console.log(arr.map((x) => x * 2));
+
+console.log(arr.reduce((acc, x) => acc + x, 0));
