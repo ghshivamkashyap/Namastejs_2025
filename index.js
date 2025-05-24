527a9ec44721248f56d686e2d242ae5448fcfe10
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-05-24 15:38:53 +0530

    Promise

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(arr.filter((x) => x >= 6));
+// console.log(arr.filter((x) => x >= 6));
 
-console.log(arr.map((x) => x * 2));
+// console.log(arr.map((x) => x * 2));
 
-console.log(arr.reduce((acc, x) => acc + x, 0));
+// console.log(arr.reduce((acc, x) => acc + x, 0));
+
+// const data = fetch("https://fakestoreapi.com/products");
+// data
+//   .then((res) => res.json())
+//   .then((products) => console.log("Products: ", products));
+
+const pro = new Promise((res, rej) => {
+  setTimeout(() => {
+    // res("Hello World");
+    rej("Something went wrong");
+  }, 2000);
+})
+  .then((message) => {
+    console.log("Result is: ",message);
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
